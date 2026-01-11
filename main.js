d68b029d4c56d3fe4f4fdf7ab67419fff15beb13
Patrick Carawan <patrickcarawan@gmail.com>
2026-01-11 14:52:29 -0500

    Animated favicon

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+const frames = [
+  "./public/p.svg",
+  "./public/d.svg",
+  "./public/c.svg"
+];
+
+let i = 0;
+setInterval(() => {
+  document.getElementById("favicon").href = frames[i];
+  i = (i + 1) % frames.length;
+}, 800);
